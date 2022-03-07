b4e3216e757057fded8ad581e4d81c621618da69
OBPSG <vbeasupergamer@gmail.com>
2022-03-07 17:17:08 -0600

    Gotten function to display the sum of all dice working

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 let diceContainer = document.getElementById("dice-container");
 let buttonContainer = document.getElementById("button-container");
 let dice = [];
+let diceSum = 0;
+let diceSumDiv = document.getElementById("dice-sum");
+let dieAddBtn = document.getElementById("die-add");
+let diceRollBtn = document.getElementById("dice-roll");
 
-class die{
-    constructor()
-    {
-        this.value = Math.floor(Math.random()*6 + 1);
+class die {
+    constructor() {
+        this.value = Math.floor(Math.random() * 6 + 1);
         this.div = document.createElement("div");
         this.div.classList.add("die");
         this.div.textContent = this.value;
         diceContainer.appendChild(this.div);
     }
 
-    roll()
-    {
-        this.value = Math.floor(Math.random()*6 + 1);
+    roll() {
+        this.value = Math.floor(Math.random() * 6 + 1);
         this.div.textContent = this.value;
     }
 }
 
-let dieAddBtn = document.getElementById("die-add");
-dieAddBtn.addEventListener("click", function()
-{
+dieAddBtn.addEventListener("click", function () {
     dice.push(new die());
+    updateSum();
 });
 
-let diceRollBtn = document.getElementById("dice-roll");
-diceRollBtn.addEventListener("click", function()
-{
-    dice.forEach(function(die) {die.roll()});
-})
+diceRollBtn.addEventListener("click", function () {
+    dice.forEach(function (die) { die.roll() });
+    updateSum();
+});
+
+function updateSum() {
+    diceSum = dice.reduce((total, die) => { return total += die.value; }, 0);
+    diceSumDiv.textContent = `Total: ${diceSum}`;
+}
 
