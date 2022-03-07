9f1b9741f09dc37bc1471acdabf8aa9d4a56206d
OBPSG <vbeasupergamer@gmail.com>
2022-03-07 14:47:39 -0600

    Gotten button to add dice working and the dice displaying as desired

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+let diceContainer = document.getElementById("dice-container");
+let buttonContainer = document.getElementById("button-container");
+let dice = [];
+
 class die{
     constructor()
     {
         this.value = Math.floor(Math.random()*6 + 1);
+        this.div = document.createElement("div");
+        this.div.classList.add("die");
+        this.div.textContent = this.value;
+        diceContainer.appendChild(this.div);
     }
 }
+
+let dieAddBtn = document.getElementById("die-add");
+dieAddBtn.addEventListener("click", function()
+{
+    dice.push(new die());
+});
