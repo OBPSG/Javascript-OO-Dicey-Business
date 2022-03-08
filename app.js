2702fe5b9b070d3de6072f91fafedd817a998d8a
OBPSG <vbeasupergamer@gmail.com>
2022-03-08 09:15:58 -0600

    Finished Lab

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ class die {
         this.div.classList.add("die");
         this.div.textContent = this.value;
         diceContainer.appendChild(this.div);
+        //Event listener to re-roll a die when it is clicked on
+        this.div.addEventListener("click", () => {
+            this.roll();
+            updateSum();
+        });
+        //Event listener to delete a die when it is double-clicked on
+        this.div.addEventListener("dblclick", () => {
+            //remove the div representing the die in the HTML
+            this.div.remove();
+            //remove the die object itself in the array
+            let index = dice.indexOf(this);
+            dice.splice(index, 1);
+
+            updateSum();
+        });
     }
 
     roll() {
@@ -31,6 +46,8 @@ diceRollBtn.addEventListener("click", function () {
     updateSum();
 });
 
+//Function that computes the total of all dice on the page
+//This function should be run whenever a die is added, removed, or re-rolled
 function updateSum() {
     diceSum = dice.reduce((total, die) => { return total += die.value; }, 0);
     diceSumDiv.textContent = `Total: ${diceSum}`;
